53bfa447c573b349a0d5200d00519b0479f3e83b
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-04-22 00:02:53 -0700

    build base application functionality

--- index.js
+++ index.js
@@ -1,13 +1,92 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  'What is the title of your project?',
+  'Please provide a brief description of the project.',
+  'What are the installation instructions?',
+  'Please provide usage instructions for the project.',
+  'How should other developers contribute to this project?',
+  'Please provide test instructions for the project.',
+  'Which license would you like to use for this project?',
+  'Please provide your github profile url.',
+  'If you would like other developers to reach out to you, please enter your email address.',
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: questions[0],
+        name: 'projTitle',
+      },
+      {
+        type: 'input',
+        message: questions[1],
+        name: 'projDesc',
+      },
+      {
+        type: 'input',
+        message: questions[2],
+        name: 'projInstall',
+      },
+      {
+        type: 'input',
+        message: questions[3],
+        name: 'projUsage',
+      },
+      {
+        type: 'input',
+        message: questions[4],
+        name: 'projContr',
+      },
+      {
+        type: 'input',
+        message: questions[5],
+        name: 'projTest',
+      },
+      {
+        type: 'list',
+        message: questions[6],
+        name: 'projLicense',
+        choices: [
+          'MIT License',
+          'GNU GPL v3',
+          'ISC License (ISC)',
+          'Creative Commons Attribution 4.0 International License',
+          'none',
+        ],
+      },
+      {
+        type: 'input',
+        message: questions[7],
+        name: 'githubProfile',
+      },
+      {
+        type: 'input',
+        message: questions[8],
+        name: 'email',
+      },
+    ])
+    .then(function (response) {
+      console.log(generateMarkdown(response));
+      fs.writeFile(
+        `${response.projTitle.split(' ').join('')}-portfolio.md`,
+        generateMarkdown(response),
+        function (err) {
+          err ? console.error(err) : console.log('Success!');
+        }
+      );
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,74 +1,105 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'none') {
+    return '';
+  } else {
+    const licenseBadgeObj = {
+      'MIT License': `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+      'GNU GPL v3': `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+      'ISC License (ISC)': `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
+      'Creative Commons Attribution 4.0 International License': `[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)`,
+    };
+    return licenseBadgeObj[license];
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'none') {
+    return '';
+  } else {
+    const licenseLinkObj = {
+      'MIT License': `https://opensource.org/licenses/MIT`,
+      'GNU GPL v3': `https://www.gnu.org/licenses/gpl-3.0`,
+      'ISC License (ISC)': `https://opensource.org/licenses/ISC`,
+      'Creative Commons Attribution 4.0 International License': `https://creativecommons.org/licensesby/4.0/`,
+    };
+    return licenseLinkObj[license];
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'none') {
+    return '';
+  } else {
+    const licenseBadge = renderLicenseBadge(license);
+    const licenseLink = renderLicenseLink(license);
+    return `
+---
+  ## License
+
+  This project is licensed under a [${license}](${licenseLink}).
+  ${licenseBadge}
+`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // const licenseBadge = renderLicenseBadge(data.projLicense);
+  // const licenseLink = renderLicenseLink(data.projLicense);
+  const licenseSection = renderLicenseSection(data.projLicense);
+  let tocLicense = '';
+  if (data.projLicense !== 'none') {
+    tocLicense = `
+  - [License](#License)`;
+  }
+  return `
+  # ${data.projTitle}
 
-`;
+  ## Description
+
+  ${data.projDesc}
+
+---
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)${tocLicense}
+  - [Questions](#Questions)
+
+---
+  ## Installation
+
+  ${data.projInstall}
+
+---
+  ## Usage
+
+  ${data.projUsage}
+
+---
+  ## Contributing
+
+  ${data.projContr}
+
+---
+  ## Tests
+
+  ${data.projTest}
+  ${licenseSection}
+---
+  ## Questions
+
+  Check out my [GitHub profile](${data.githubProfile}).
+
+  For additional questions about this project, please reach out to me at <${data.email}>`;
 }
 
 module.exports = generateMarkdown;
-
-// the below template literal is my first attempt at building out the Readme template.  I used the README.md from a previous project as a starting point.
-/*
-
-`# PROJECT_TITLE
-
-## Purpose and Functionality
-
-DESCRIPTION_TEXT
-
----
-
-## Key Learnings
-
-KEY_LEARNING_TEXT
-
----
-
-## Deployed Site
-
-The deployed site is hosted on github pages at the following link:
-
-DEPLOYED_SITE_LINK
-
----
-
-## Screenshots
-
-SCREENSHOT_DESCRIPTION
-
-![SCREENSHOT_ALT_TEXT](SCREENSHOT_PATH)
-
----
-
-## Known Bugs
-
-BUG_LIST
-
----
-
-## Future Improvements
-
-IMPROVEMENT_LIST
-
----
-
-## License
-
-LICENSE_INFO
-
----
-`;
-
-*/
